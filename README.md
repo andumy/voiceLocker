ffbc87931d7e24416fdae929eec37eb34d7950d9
Andrei Dumitrescu <andrei_dumitrescu_96@yahoo.com>
2024-10-28 02:46:45 +0000

    initial commit. Locking/unlocking functional. Must add restriction to allow only users in channel to be able to call the comamnd, auto unlock if channel empty, autocomplete with room numbers

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # voiceLocker
+Discord bot for locking/unlocking voice rooms that follows the naming convention `Push <index>`
---
+++ main.py
@@ -0,0 +1,71 @@
+import os
+
+import interactions
+from interactions import GuildVoice, OptionType, SlashContext, slash_command, slash_option
+
+bot = interactions.Client(token=os.environ['DISCORD_BOT_TOKEN'],
+                          intents=interactions.Intents.GUILDS
+                          | interactions.Intents.GUILD_VOICE_STATES)
+
+interactions.listen()
+
+
+async def on_startup():
+  print("Bot is ready!")
+
+
+@slash_command(
+    name="lock_voice",
+    description="Lock a voice channel",
+)
+@slash_option(name="push_id",
+              description="Push Channel Id",
+              required=True,
+              opt_type=OptionType.INTEGER)
+async def lock_voice(ctx: SlashContext, push_id: int):
+  await toggleChannel(ctx, push_id, False)
+  await ctx.send("Channel 'Push " + str(push_id) + "' is now locked.")
+
+
+@slash_command(
+    name="unlock_voice",
+    description="Unlock a voice channel",
+)
+@slash_option(name="push_id",
+              description="Push Channel Id",
+              required=True,
+              opt_type=OptionType.INTEGER)
+async def unlock_voice(ctx: SlashContext, push_id: int):
+  await toggleChannel(ctx, push_id, True)
+  await ctx.send("Channel 'Push " + str(push_id) + "' is now unlocked.")
+
+
+async def toggleChannel(ctx: SlashContext, push_id: int, can_connect: bool):
+  await ctx.defer()
+
+  if (ctx.guild_id is None):
+    await ctx.send("Internal error: g100")
+    return
+
+  guild = bot.get_guild(ctx.guild_id)
+  if (guild is None):
+    await ctx.send("Internal error: g101")
+    return
+
+  selected_channel = None
+  for channel in guild.channels:
+    if (channel.name != "Push " + str(push_id)):
+      continue
+
+    if (type(channel) is GuildVoice):
+      selected_channel = channel
+
+  if (selected_channel is None):
+    await ctx.send("Internal error: c100")
+    return
+
+  await selected_channel.set_permission(connect=can_connect,
+                                       target=guild.default_role)
+
+
+bot.start()
